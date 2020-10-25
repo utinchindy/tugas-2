b565b0584347c2cafd1c79aad8f7e7341980e31d
utinchindy <71062640+utinchindy@users.noreply.github.com>
2020-10-25 12:59:58 +0700

    Initial commit

---
+++ system/resources/views/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="widht=device-width, initial-scale">
+	<link href="assets/css/style.css" rel="stylesheet">
+	<title>Login LK</title>
+</head>
+	<body id="bg-login">
+		<div class="log-1">
+			<h2>Login</h2>
+			<form action="" method="">
+				<input type="text" name="user" placeholder="Username" class="input-control"> 
+				<input type="password" name="user" placeholder="password" class="input-control"> 
+				<button type=""><a href="{{ url ('/index')}}">Login</a></button>
+			</form>
+		</div>
+	</body>
+	</html>
